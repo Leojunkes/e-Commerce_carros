65c842d7129474e233340ba82acdef027e6cc984
leonardo <leojn8@gmail.com>
2021-02-18 02:08:11 -0300

    redux/actionsparte 2

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,32 +1,55 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { Container } from './styles';
 
+import { addCar } from '../../store/cars';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 function Add() {
+  const [form, setForm] = useState({ name: '', url: '' });
+  const d = useDispatch();
+
+  function formChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    d(addCar(form));
+    console.log(form);
+    localStorage.setItem('cars', JSON.stringify(form));
+    setForm(form);
+  }
   return (
     <Container>
-      <form className="container-mt-5">
+      <form className="container-mt-5" onSubmit={onSubmit}>
         <div className="form-group">
           <label>Nome</label>
           <input
+            onChange={formChange}
             type="text"
-            name="Nome..."
+            name="name"
             className="form-control"
-            placeholder="Url"
+            placeholder="Nome"
+            value={form.name}
           ></input>
         </div>
         <div className="form-group">
           <label>Url:</label>
           <input
+            onChange={formChange}
             type="text"
             name="url"
             className="form-control"
             placeholder="Url"
+            value={form.url}
           ></input>
         </div>
-        <button type="submit" className="btn btn-primary">
-          Adicionar
-        </button>
+        <Link to="/list">
+          <button type="submit" className="btn btn-primary">
+            Adicionar
+          </button>
+        </Link>
       </form>
     </Container>
   );
--- src/store/cars/index.js
+++ src/store/cars/index.js
@@ -1,9 +1,4 @@
-const INITIAL_STATE=[
-    {
-    name: 'Audi',
-    url:
-      'https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  },
+const INITIAL_STATE = [
   {
     name: 'Audi',
     url:
@@ -14,9 +9,22 @@ const INITIAL_STATE=[
     url:
       'https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   },
-]
+  {
+    name: 'Audi',
+    url:
+      'https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+];
 
-export default function reducer(state=INITIAL_STATE, action) {
-    
+export default function reducer(state = INITIAL_STATE, action) {
+  if (action.type === 'ADD_CAR') {
+    return [...state, action.car];
+  }
   return state;
 }
+export const addCar = (car) => {
+  return {
+    type: 'ADD_CAR',
+    car,
+  };
+};
